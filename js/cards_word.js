642dcc5db8958f830834ece4ee6b5f408b319960
Svadhis <imeian@gmail.com>
2019-05-03 17:50:48 +0200

    fixed cards_words.js length

--- js/cards_word.js
+++ js/cards_word.js
@@ -1,52 +1,153 @@
 var cardsWord = [
-	{
-		french: 'pour résumer la télé-réalité',
-		english: '',
-		spanish: ''
-	},
-	{
-		french: "qui vexerait n'importe quelle femme",
-		english: '',
-		spanish: ''
-	},
-	{
-		french: 'pour décrire Donald Trump',
-		english: '',
-		spanish: ''
-	},
-	{
-		french: "d'au moins 4 syllabes",
-		english: '',
-		spanish: ''
-	},
-	{
-		french: 'pour déclarer sa flamme',
-		english: '',
-		spanish: ''
-	},
-	{
-		french: 'pour résumer Star Wars',
-		english: '',
-		spanish: ''
-	},
-	{
-		french: 'en espagnol',
-		english: '',
-		spanish: ''
-	},
-	{
-		french: 'qui veut rien dire du tout',
-		english: '',
-		spanish: ''
-	},
-	{
-		french: 'qui rime avec peinture',
-		english: '',
-		spanish: ''
-	},
-	{
-		french: 'qui soit le mot "mot"',
-		english: '',
-		spanish: ''
-	}
+    {
+        french: "pour résumer la télé-réalité",
+        english: "",
+        spanish: ""
+    },
+    {
+        french: "qui vexerait n'importe quelle femme",
+        english: "",
+        spanish: ""
+    },
+    {
+        french: "pour décrire Donald Trump",
+        english: "",
+        spanish: ""
+    },
+    {
+        french: "d'au moins 4 syllabes",
+        english: "",
+        spanish: ""
+    },
+    {
+        french: "pour déclarer sa flamme",
+        english: "",
+        spanish: ""
+    },
+    {
+        french: "pour résumer Star Wars",
+        english: "",
+        spanish: ""
+    },
+    {
+        french: "en espagnol",
+        english: "",
+        spanish: ""
+    },
+    {
+        french: "qui veut rien dire du tout",
+        english: "",
+        spanish: ""
+    },
+    {
+        french: "qui rime avec peinture",
+        english: "",
+        spanish: ""
+    },
+    {
+        french: 'qui soit le mot "mot"',
+        english: "",
+        spanish: ""
+    },
+    //Repeat
+    {
+        french: "pour résumer la télé-réalité",
+        english: "",
+        spanish: ""
+    },
+    {
+        french: "qui vexerait n'importe quelle femme",
+        english: "",
+        spanish: ""
+    },
+    {
+        french: "pour décrire Donald Trump",
+        english: "",
+        spanish: ""
+    },
+    {
+        french: "d'au moins 4 syllabes",
+        english: "",
+        spanish: ""
+    },
+    {
+        french: "pour déclarer sa flamme",
+        english: "",
+        spanish: ""
+    },
+    {
+        french: "pour résumer Star Wars",
+        english: "",
+        spanish: ""
+    },
+    {
+        french: "en espagnol",
+        english: "",
+        spanish: ""
+    },
+    {
+        french: "qui veut rien dire du tout",
+        english: "",
+        spanish: ""
+    },
+    {
+        french: "qui rime avec peinture",
+        english: "",
+        spanish: ""
+    },
+    {
+        french: 'qui soit le mot "mot"',
+        english: "",
+        spanish: ""
+    },
+    {
+        french: "pour résumer la télé-réalité",
+        english: "",
+        spanish: ""
+    },
+    {
+        french: "qui vexerait n'importe quelle femme",
+        english: "",
+        spanish: ""
+    },
+    {
+        french: "pour décrire Donald Trump",
+        english: "",
+        spanish: ""
+    },
+    {
+        french: "d'au moins 4 syllabes",
+        english: "",
+        spanish: ""
+    },
+    {
+        french: "pour déclarer sa flamme",
+        english: "",
+        spanish: ""
+    },
+    {
+        french: "pour résumer Star Wars",
+        english: "",
+        spanish: ""
+    },
+    {
+        french: "en espagnol",
+        english: "",
+        spanish: ""
+    },
+    {
+        french: "qui veut rien dire du tout",
+        english: "",
+        spanish: ""
+    },
+    {
+        french: "qui rime avec peinture",
+        english: "",
+        spanish: ""
+    },
+    {
+        french: 'qui soit le mot "mot"',
+        english: "",
+        spanish: ""
+    }
 ];
